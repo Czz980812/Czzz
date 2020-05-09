ddc8ff71cc72335bb4af4e964cad64b8143be7bf
czz812 <2295338276@qq.com>
2020-05-09 14:29:35 +0800

    傻逼玩意

--- BbsDetail.java
+++ BbsDetail.java
@@ -1,36 +1,5 @@
-package cn.kgc.entity;
-
-import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import tk.mybatis.mapper.annotation.KeySql;
-
-import javax.persistence.Column;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import java.io.Serializable;
-import java.util.Date;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-
-@Table(name = "bbs_detail")
-public class BbsDetail implements Serializable {
-
-    @Id
-    @KeySql(useGeneratedKeys = true)
-    private Integer id;
-    @Column(name = "sortId")
-    private Integer sortId;
-    private String title;
-    private String detail;
-    private String author;
-    @JsonFormat(pattern = "yyyy-MM-dd")
-    @Column(name = "createDate")
-    private Date createDate;
-    @Column(name = "replyCount")
-    private Integer replyCount;
+public class BbsDetail(){
+     System.out.println("哈哈哈 ")
 
 }
+
